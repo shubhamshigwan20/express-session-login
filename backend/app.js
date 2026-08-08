97e3126749f67f317e1723fd1fc437e52e27f7a4
Shubham Shigwan <shubhamonline024@gmail.com>
2026-08-08 14:11:16 +0530

    added cors bug fix

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 80;
 
 app.use(helmet());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
